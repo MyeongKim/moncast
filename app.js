fa7612054619a987f1c1f54584635cce9f43c515
mingkim <ming3772@gmail.com>
2015-05-23 03:24:03 +0900

    simple input html for login

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var bodyParser = require('body-parser')
 var io = require('socket.io').listen(server);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 // router
 app.get('/', function (req, res) {
 	res.render('index');
+});
+
+app.get('/new', function (req, res) {
+	res.render('new');
+});
+
+app.post('/login', function (req, res){
+	console.log("user login==" + "id : " + req.body.id + "pw : " + req.body.pw);
+	res.end();
 });
 
 io.sockets.on('connection', function (socket) {
